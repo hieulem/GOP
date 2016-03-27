9fe5e491202eec9c46a28b7ef58684b6571280ca
hieule <lmhieu612@gmail.com>
2016-03-26 22:24:16 -0400

    delete

--- code/matgraph-2.0/matgraph/@graph/clear_labels.m
+++
@@ -1,6 +0,0 @@
-function clear_labels(g)
-% clearl_labels(g) --- delete all labels in g
-
-global GRAPH_MAGIC;
-GRAPH_MAGIC.graphs{g.idx}.labels = cell(0);
-rmfield(GRAPH_MAGIC.graphs{g.idx},'labels');
--- code/matgraph-2.0/matgraph/@graph/cycle.m
+++
@@ -1,8 +0,0 @@
-function cycle(g,n)
-% cycle(g,n) --- create a cycle graph on n vertices
-
-global GRAPH_MAGIC
-
-path(g,n);
-add(g,1,n);
-embed(g);
