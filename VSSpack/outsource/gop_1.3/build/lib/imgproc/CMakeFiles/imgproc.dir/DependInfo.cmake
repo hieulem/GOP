19e32e89282fedd6ff10f1490b1540d195f57ffd
hieule <lmhieu612@gmail.com>
2016-03-31 21:21:51 -0400

    add lib for wrapper

---
+++ VSSpack/outsource/gop_1.3/build/lib/imgproc/CMakeFiles/imgproc.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/nfs/bigeye/hieule/GOP/outsource/gop_1.3/lib/imgproc/color.cpp" "/nfs/bigeye/hieule/GOP/outsource/gop_1.3/build/lib/imgproc/CMakeFiles/imgproc.dir/color.cpp.o"
+  "/nfs/bigeye/hieule/GOP/outsource/gop_1.3/lib/imgproc/filter.cpp" "/nfs/bigeye/hieule/GOP/outsource/gop_1.3/build/lib/imgproc/CMakeFiles/imgproc.dir/filter.cpp.o"
+  "/nfs/bigeye/hieule/GOP/outsource/gop_1.3/lib/imgproc/gradient.cpp" "/nfs/bigeye/hieule/GOP/outsource/gop_1.3/build/lib/imgproc/CMakeFiles/imgproc.dir/gradient.cpp.o"
+  "/nfs/bigeye/hieule/GOP/outsource/gop_1.3/lib/imgproc/image.cpp" "/nfs/bigeye/hieule/GOP/outsource/gop_1.3/build/lib/imgproc/CMakeFiles/imgproc.dir/image.cpp.o"
+  "/nfs/bigeye/hieule/GOP/outsource/gop_1.3/lib/imgproc/morph.cpp" "/nfs/bigeye/hieule/GOP/outsource/gop_1.3/build/lib/imgproc/CMakeFiles/imgproc.dir/morph.cpp.o"
+  "/nfs/bigeye/hieule/GOP/outsource/gop_1.3/lib/imgproc/nms.cpp" "/nfs/bigeye/hieule/GOP/outsource/gop_1.3/build/lib/imgproc/CMakeFiles/imgproc.dir/nms.cpp.o"
+  "/nfs/bigeye/hieule/GOP/outsource/gop_1.3/lib/imgproc/resample.cpp" "/nfs/bigeye/hieule/GOP/outsource/gop_1.3/build/lib/imgproc/CMakeFiles/imgproc.dir/resample.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "LBFGS_FLOAT=32"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/nfs/bigeye/hieule/GOP/outsource/gop_1.3/build/lib/util/CMakeFiles/util.dir/DependInfo.cmake"
+  "/nfs/bigeye/hieule/GOP/outsource/gop_1.3/build/external/liblbfgs-1.10/CMakeFiles/lbfgs.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include/eigen3"
+  "../external"
+  "../external/CImg"
+  "../external/rapidxml-1.13"
+  "../external/libsvm-3.17"
+  "../external/liblbfgs-1.10/include"
+  "../external/maxflow-v3.03.src"
+  "../external/ibfs"
+  "../external/quadprog"
+  "../lib"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
