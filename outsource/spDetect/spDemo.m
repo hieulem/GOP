6d133072f6f375252d0d6a44166ef6c67366bf7d
hieule <lmhieu612@gmail.com>
2016-04-05 14:26:04 -0400

    add lib spd

--- outsource/spDetect/spDemo.m
+++ outsource/spDetect/spDemo.m
@@ -1,7 +1,5 @@
 % Demo for Sticky Superpixels (please see readme.txt first).
 
-addpath(genpath('/Users/hieule/Downloads/GOP/outsource/'));
-clear;
 %% load pre-trained edge detection model and set opts (see edgesDemo.m)
 model=load('models/forest/modelBsds'); model=model.model;
 model.opts.nms=-1; model.opts.nThreads=4;
@@ -28,5 +26,5 @@ figure(3); im(1-U); return;
 %% compute video superpixels reusing initialization from previous frame
 Is=seqIo(which('peds30.seq'),'toImgs'); Vs=single(Is); opts.bounds=0; tic
 for i=1:size(Is,4), I=Is(:,:,:,i); E=edgesDetect(I,model);
-  [opts.seed,Vs(:,:,:,i)]=spDetect(I,E,opts); end;  toc;%opts.seed=[];
+  [opts.seed,Vs(:,:,:,i)]=spDetect(I,E,opts); end; opts.seed=[]; toc
 Vs=uint8(Vs*255); playMovie([Is Vs],15,-10,struct('hasChn',1))
