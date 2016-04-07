78ad9f85b84ad8ae3d87319d3bc410ca601e17db
hieule <lmhieu612@gmail.com>
2016-04-06 21:41:04 -0400

    add computeflow

---
+++ code/computeflow/computeflow_vbs.m
@@ -0,0 +1,27 @@
+% clear;
+% addpath(genpath('.'));
+% addpath(genpath('../outsource/'));
+% %addpath(genpath('../outsource/spDetect'));
+% dataset_path = '../../video/vsb100/Test/';
+% all_path= dir(dataset_path);all_path(1:2) = [];
+% for i=1:length(all_path)
+%     video_info{i}.path = [dataset_path,all_path(i).name];
+%     video_info{i}.name = all_path(i).name;
+% end
+% save('video_info','video_info');
+load('video_info');
+
+i=1;
+video_name = video_info{i}.name;
+flowpath = ['../../flow_data/flow_motion_default/vbs100/test/' video_name];
+flowfile = ['flow',video_name];
+video_path = video_info{i}.path;
+a=pwd;cd(video_path);imlist = dir('*.png');
+tmp = imresize(imread(imlist(1).name),0.5);
+img = zeros(size(tmp,1),size(tmp,2),3,length(imlist));
+for j=1:length(imlist)
+    img(:,:,:,j) = imresize(imread(imlist(j).name),0.5);
+end
+cd(a);
+[motionboundaries,~] = computeflowandmotionb( img,flowpath,flowfile );
+
---
+++ code/computeflow/computeflowandmotionb.m
@@ -0,0 +1,26 @@
+function [ motionboundaries,flow ] = computeflowandmotionb( img,savepath,filename )
+%COMPUTEFLOWANDMOTIONB Summary of this function goes here
+%   Detailed explanation goes here
+if ~exist(savepath)
+    mkdir(savepath)
+end;
+if exist(fullfile(savepath,filename))
+    load(fullfile(savepath,filename));
+else
+    
+    [h,w,~,numi] = size(img);
+    img(:,:,:,numi+1) =  img(:,:,:,numi-1);
+    flow = zeros(h,w,2,numi);
+    bflow = zeros(h,w,2,numi);
+    motionboundaries =zeros(h,w,numi);
+    
+    parfor i =1:numi
+        i
+        flow(:,:,:,i)= estimate_flow_interface(img(:,:,:,i), img(:,:,:,i+1), 'classic+nl-fast');
+        motionboundaries(:,:,i)= detect_motionboundaries(img(:,:,:,i),flow(:,:,:,i),img(:,:,:,i+1));
+    end 
+    save(fullfile(savepath,filename),'flow','motionboundaries');
+end
+
+
+
