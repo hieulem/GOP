5862f03b212b7adbd551ced96b132d0dfc7ae8f5
lmhieu612 <lmhieu612@gmail.com>
2016-04-08 16:19:13 -0400

    fix shell script for VSS flow code

--- code/computeflow/flowjob.sh
+++ code/computeflow/flowjob.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-SGE_TASK_NUM=$1
+SGE_TASK_ID=$1
 cd /nfs/bigeye/hieule/GOP/GOP/code/computeflow
-/usr/local/MATLAB/R2015b/bin/matlab -nodisplay -r "vbs(${SGE_TASK_ID}); exit;" -logfile "vbs_${SGE_TASK_ID}.txt"
+/usr/local/MATLAB/R2015b/bin/matlab -nodisplay -r "computeflow_vbs(${SGE_TASK_ID}); exit;" -logfile "vbs_${SGE_TASK_ID}.txt"
 
 exit 0;
 
