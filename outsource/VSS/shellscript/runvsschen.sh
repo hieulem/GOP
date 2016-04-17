7f3ad133247dccfa94978afc82582a56c46329d4
lmhieu612 <lmhieu612@gmail.com>
2016-04-16 23:23:31 -0400

    reorganize stuff

---
+++ outsource/VSS/shellscript/runvsschen.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+JOB_NUM=8
+JOB_PARALLEL=8
+JOB_MEM=30G
+
+qsub -cwd -t 1-${JOB_NUM} -tc ${JOB_PARALLEL} -l mem_free=${JOB_MEM} -m ea -o ./co.txt -e ./ce.txt ./chen_baseline.sh
+${JOB_NUM}
+
+exit 0;
+
